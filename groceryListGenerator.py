fbb670d944056aa282ce90d470d63ab6fab69f06
Gianna-LaVanway <gianna.lavanway@gmail.com>
2025-09-12 15:33:06 +0000

    Started making window

--- groceryListGenerator.py
+++ groceryListGenerator.py
@@ -1,2 +1,17 @@
 import tkinter as tk
+import tkinter.messagebox as msgbox
 import urllib.request as req
+
+
+
+def main():
+    # Create Window
+    root = tk.Tk()
+    root.title("Grocery List Generator")
+
+
+
+    # Display Window (will stop the execution of any code after this point until the window is closed)
+    root.mainloop()
+
+main()
