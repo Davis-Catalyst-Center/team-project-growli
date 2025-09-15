8da8014b4b523acd5399590f52a093b23b4c5f56
Gianna-LaVanway <gianna.lavanway@gmail.com>
2025-09-15 16:15:07 +0000

    added textbox and returns input

--- groceryListGenerator.py
+++ groceryListGenerator.py
@@ -3,17 +3,44 @@ import tkinter.messagebox as msgbox
 import urllib.request as req
 
 
+# Having issues with DISPLAY that are only occuring when using GitHub
+
+entryIngredient = None
+labelList = None
+
+def entered():
+    global entryIngredient
+    ingredient = entryIngredient.get()
+    labelListText = labelList.cget("text")
+    if labelListText != "":
+        labelList.configure(text=labelListText + "\n" + ingredient)
+    else:
+        labelList.configure(text=ingredient)
+
+    entryIngredient.delete(0, "end")
+
 def main():
     # Create Window
     root = tk.Tk()
     root.title("Grocery List Generator")
+    root.geometry("1000x500")
 
+    # label for instructions
+    labelInstructions = tk.Label(root, text="Please enter your ingredients")
+    labelInstructions.pack()
 
     # textbox for input
-    entry_ingredient = tk.Entry(root)
-    entry_ingredient.pack()
+    global entryIngredient
+    entryIngredient = tk.Entry(root)
+    entryIngredient.pack()
+
+    # List
+    global labelList
+    labelList = tk.Label(root, text="")
+    labelList.pack()
 
 
+    root.bind(("<Return>") ,lambda event:entered())
 
     # Display Window (will stop the execution of any code after this point until the window is closed)
     root.mainloop()
