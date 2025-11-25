b028fd4f9eae66fbc572ef3e0c975408389c6d11
cytiscortalis <26ggardner@go.dsdmail.net>
2025-11-25 15:20:39 +0000

    why scroll why

--- groceryListGenerator.py
+++ groceryListGenerator.py
@@ -382,7 +382,10 @@ def entered():
     alphabetizedThings = alphabetizeList(allThings)
     combined = combine_ingredients(alphabetizedThings)
     lines = [f"{it.quantity} {it.unit} {it.name}".strip() for it in combined]
-    labelList.configure(text="\n".join(lines))
+        labelList.config(state="normal")
+        labelList.delete("1.0", tk.END)
+        labelList.insert(tk.END, "\n".join(lines))
+        labelList.config(state="disabled")
     entryLink.delete(0, "end")
 
 def linkButtonClicked(buttonUrl, linkIndex):
